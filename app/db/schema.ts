718e81f00daaf082f46732890e57b764cb1f1366
mauriciorobayo-jh <rfmajo@gmail.com>
2025-01-08 15:58:46 -0500

    add index

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,5 +1,5 @@
 import { sql } from "drizzle-orm";
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { index, int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const tokens = sqliteTable("tokens", {
   id: int().primaryKey({ autoIncrement: true }),
@@ -8,30 +8,36 @@ export const tokens = sqliteTable("tokens", {
     .notNull()
     .default(sql`(unixepoch())`),
 });
-export const companies = sqliteTable("companies", {
-  id: int().primaryKey({ autoIncrement: true }),
-  nit: int().unique().notNull(),
-  documentType: text().notNull(),
-  businessName: text().notNull(),
-  category: text().notNull(),
-  legalEntity: text().notNull(),
-  registrationDate: int({ mode: "timestamp" }).notNull(),
-  ruesSyncId: int().references(() => ruesSync.id),
-  businessAddress: text().notNull(),
-  companySize: int().notNull(),
-  economicActivity1: text().notNull(),
-  registrationId: int(),
-  economicActivity2: text(),
-  economicActivity3: text(),
-  economicActivity4: text(),
-  chamberCode: int().references(() => chambers.code),
-  registrationNumber: int(),
-  legalOrganization: text(),
-  lastRenewedYear: int(),
-  isActive: int({ mode: "boolean" }),
-  city: text(),
-  state: text(),
-});
+export const companies = sqliteTable(
+  "companies",
+  {
+    id: int().primaryKey({ autoIncrement: true }),
+    nit: int().unique().notNull(),
+    documentType: text().notNull(),
+    businessName: text().notNull(),
+    category: text().notNull(),
+    legalEntity: text().notNull(),
+    registrationDate: int({ mode: "timestamp" }).notNull(),
+    ruesSyncId: int().references(() => ruesSync.id),
+    businessAddress: text().notNull(),
+    companySize: int().notNull(),
+    economicActivity1: text().notNull(),
+    registrationId: int(),
+    economicActivity2: text(),
+    economicActivity3: text(),
+    economicActivity4: text(),
+    chamberCode: int().references(() => chambers.code),
+    registrationNumber: int(),
+    legalOrganization: text(),
+    lastRenewedYear: int(),
+    isActive: int({ mode: "boolean" }),
+    city: text(),
+    state: text(),
+  },
+  (table) => {
+    return [index("rues_sync_idx").on(table.ruesSyncId)];
+  }
+);
 export const chambers = sqliteTable("chambers", {
   id: int().primaryKey({ autoIncrement: true }),
   code: int().unique().notNull(),
