7524bab25954924c8e19083e7fabfc08485206a6
mauriciorobayo-jh <rfmajo@gmail.com>
2025-01-08 23:25:53 -0500

    fix seed

--- app/db/seed/index.ts
+++ app/db/seed/index.ts
@@ -2,7 +2,7 @@ import { seedChambers } from "@/app/db/seed/chambers";
 import { seedCompanies } from "@/app/db/seed/companies";
 
 async function main() {
-  await Promise.all([seedChambers(), seedCompanies]);
+  await Promise.all([seedChambers(), seedCompanies()]);
   console.log("done!");
 }
 main();
