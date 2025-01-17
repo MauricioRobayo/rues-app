7b3802f9e27400288428c3fa3e4eb98c60d8c7d3
mauriciorobayo-jh <rfmajo@gmail.com>
2025-01-17 00:50:47 -0500

    refactor

--- app/(search)/actions/search.ts
+++ app/(search)/actions/search.ts
@@ -1,13 +1,9 @@
 "use server";
 
 import { getToken } from "@/app/[company]/services/rues";
+import { verifyRecaptcha } from "@/app/lib/verify-recaptcha";
 import { RUES } from "@mauriciorobayo/rues-api";
 
-const projectID = process.env.GCP_PROJECT_ID ?? "";
-const secretKey = process.env.RECAPTCHA_SECRET_KEY ?? "";
-const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? "";
-
-const verificationUrl = `https://recaptchaenterprise.googleapis.com/v1/projects/${projectID}/assessments?key=${secretKey}`;
 const rues = new RUES();
 
 export async function search({
@@ -20,7 +16,7 @@ export async function search({
   if (!companyName) {
     return null;
   }
-  if (!(await verifyRecaptcha(recaptchaToken))) {
+  if (!(await verifyRecaptcha({ token: recaptchaToken, action: "SEARCH" }))) {
     return null;
   }
 
@@ -35,49 +31,4 @@ export async function search({
   return (response.data.registros ?? [])
     .filter((registro) => Boolean(Number(registro.nit)))
     .slice(0, 25);
-}
-
-async function verifyRecaptcha(token: string) {
-  try {
-    const response = await fetch(verificationUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        event: {
-          token,
-          expectedAction: "SEARCH",
-          siteKey,
-        },
-      }),
-    });
-    const data = await response.json();
-
-    console.log("response", JSON.stringify(data, null, 2));
-
-    if (!data.tokenProperties.valid) {
-      console.log(
-        `The CreateAssessment call failed because the token was: ${data.tokenProperties.invalidReason}`,
-      );
-      return false;
-    }
-
-    if (data.tokenProperties.action !== "SEARCH") {
-      console.log(
-        "The action attribute in your reCAPTCHA tag does not match the action you are expecting to score",
-      );
-      return false;
-    }
-
-    if (!data.riskAnalysis?.score) {
-      console.log("Failed to get score");
-      return false;
-    }
-
-    return (data.riskAnalysis.score ?? 0) >= 0.5;
-  } catch (err) {
-    console.error(err);
-    return false;
-  }
 }
---
+++ app/lib/verify-recaptcha.ts
@@ -0,0 +1,55 @@
+const projectID = process.env.GCP_PROJECT_ID ?? "";
+const secretKey = process.env.RECAPTCHA_SECRET_KEY ?? "";
+const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? "";
+
+const verificationUrl = `https://recaptchaenterprise.googleapis.com/v1/projects/${projectID}/assessments?key=${secretKey}`;
+
+export async function verifyRecaptcha({
+  token,
+  action,
+}: {
+  token: string;
+  action: string;
+}) {
+  try {
+    const response = await fetch(verificationUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        event: {
+          token,
+          siteKey,
+        },
+      }),
+    });
+    const data = await response.json();
+
+    console.log("response", JSON.stringify(data, null, 2));
+
+    if (!data.tokenProperties.valid) {
+      console.log(
+        `The CreateAssessment call failed because the token was: ${data.tokenProperties.invalidReason}`,
+      );
+      return false;
+    }
+
+    if (data.tokenProperties.action !== action) {
+      console.log(
+        "The action attribute in your reCAPTCHA tag does not match the action you are expecting to score",
+      );
+      return false;
+    }
+
+    if (!data.riskAnalysis?.score) {
+      console.log("Failed to get score");
+      return false;
+    }
+
+    return (data.riskAnalysis.score ?? 0) >= 0.5;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+}
