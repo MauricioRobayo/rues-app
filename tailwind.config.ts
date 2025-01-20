27da3b9721b0cd9a99fb7906ca9053fff23e5766
mauriciorobayo-jh <rfmajo@gmail.com>
2025-01-20 16:16:15 -0500

    delete unused theme option

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,7 @@
 import type { Config } from "tailwindcss";
-import colors from "tailwindcss/colors";
 
 export default {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {
-    extend: {
-      colors: {
-        brand: colors.sky[700],
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 } satisfies Config;
