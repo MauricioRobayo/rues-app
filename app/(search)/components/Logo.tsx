042f6b48ab7b5bf0cb0b39e6594196839b792c96
mauriciorobayo-jh <rfmajo@gmail.com>
2025-01-20 16:15:45 -0500

    proper logo font

--- app/(search)/components/Logo.tsx
+++ app/(search)/components/Logo.tsx
@@ -1,8 +1,9 @@
+import { montserrat } from "@/app/fonts";
 import { Text } from "@radix-ui/themes";
 
 export function Logo() {
   return (
-    <div className="font-brand">
+    <div className={montserrat.className}>
       <Text size="2">registro</Text>
       <Text size="4" weight="bold">
         NIT
