c6671b954364da020d799064c3c81814bf0c524c
Mauricio Robayo <rfmajo@gmail.com>
2025-01-30 23:20:13 -0500

    restore file

---
+++ app/shared/component/CompanyCard.tsx
@@ -0,0 +1,31 @@
+import { CompanyStatusBadge } from "@/app/shared/component/CompanyStatusBadge";
+import { Card, Heading, Flex, type CardProps } from "@radix-ui/themes";
+
+export interface CompanySummary {
+  name: string;
+  fullNit: string;
+  isActive: boolean;
+  slug: string;
+  nit: string;
+}
+
+export function CompanyCard({
+  name,
+  fullNit,
+  isActive,
+  ...props
+}: CompanySummary & CardProps) {
+  return (
+    <Card {...props}>
+      <Heading itemProp="name" color="sky">
+        {name}
+      </Heading>
+      <Flex align="center" gap="2">
+        <Heading as="h2" size="4" itemProp="taxID">
+          NIT: {fullNit}
+        </Heading>
+        <CompanyStatusBadge isActive={isActive} />
+      </Flex>
+    </Card>
+  );
+}
