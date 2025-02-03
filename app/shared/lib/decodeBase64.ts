2749fb168cda3ff5a4ecd7cd58033c0ebf024d2e
Mauricio Robayo <rfmajo@gmail.com>
2025-02-03 13:55:25 -0500

    try more encodings (#23)
    * try more encodings
    
    * fix

--- app/shared/lib/decodeBase64.ts
+++ app/shared/lib/decodeBase64.ts
@@ -2,10 +2,18 @@ import jschardet from "jschardet";
 import iconv from "iconv-lite";
 
 export function decodeBase64(base64Text?: string) {
-  if (!base64Text) {
-    return null;
+  const buffer = Buffer.from(base64Text ?? "", "base64");
+  const encodings = jschardet.detectAll(buffer);
+
+  for (const { encoding, confidence } of encodings) {
+    if (confidence < 0.75) {
+      return "";
+    }
+    try {
+      const decoded = iconv.decode(buffer, encoding).trim();
+      return decoded;
+    } catch {}
   }
-  const buffer = Buffer.from(base64Text, "base64");
-  const { encoding } = jschardet.detect(buffer) ?? "ISO-8859-1";
-  return iconv.decode(buffer, encoding).trim();
+
+  return null;
 }
