b7b93029d0494e5dc8a1c1358817c7d58f74e3c0
Mauricio Robayo <rfmajo@gmail.com>
2025-02-03 13:47:17 -0500

    fix

--- app/shared/lib/decodeBase64.ts
+++ app/shared/lib/decodeBase64.ts
@@ -2,11 +2,8 @@ import jschardet from "jschardet";
 import iconv from "iconv-lite";
 
 export function decodeBase64(base64Text?: string) {
-  if (!base64Text) {
-    return null;
-  }
-  const buffer = Buffer.from(base64Text, "base64");
-  const encodings = jschardet.detectAll(buffer) ?? "ISO-8859-1";
+  const buffer = Buffer.from(base64Text ?? "", "base64");
+  const encodings = jschardet.detectAll(buffer);
 
   for (const { encoding, confidence } of encodings) {
     if (confidence < 0.75) {
@@ -17,4 +14,6 @@ export function decodeBase64(base64Text?: string) {
       return decoded;
     } catch {}
   }
+
+  return null;
 }
