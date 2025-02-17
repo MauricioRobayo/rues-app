872963e0edf2f8d70cbf5170c4881faf810a085c
Mauricio Robayo <rfmajo@gmail.com>
2025-02-17 00:08:55 -0500

    Fix decoding (#64)
    * keep decoding if ascii
    
    * don't know if it is an improvement
    
    * allow more depth just in case
    
    * triple equals
    
    * encoding log

--- app/lib/decodeBase64.ts
+++ app/lib/decodeBase64.ts
@@ -1,19 +1,30 @@
 import jschardet from "jschardet";
 import iconv from "iconv-lite";
 
-export function decodeBase64(base64Text?: string) {
-  const buffer = Buffer.from(base64Text ?? "", "base64");
+const MAX_DEPTH = 5;
+
+export function decodeBase64(str?: string, depth = 0) {
+  if (!str || depth === MAX_DEPTH) {
+    return str;
+  }
+
+  const buffer = Buffer.from(str, "base64");
   const encodings = jschardet.detectAll(buffer);
 
   for (const { encoding, confidence } of encodings) {
     if (confidence < 0.75) {
-      return "";
+      return str;
     }
     try {
-      const decoded = iconv.decode(buffer, encoding).trim();
-      return decoded;
-    } catch {}
+      const decoded = iconv.decode(buffer, encoding);
+      if (encoding === "ascii" && confidence > 0.85) {
+        return decodeBase64(decoded, depth + 1);
+      }
+      return decoded.trim();
+    } catch (err) {
+      console.error("decodeBase64 decoding failed:", encoding, str, err);
+    }
   }
 
-  return null;
+  return str;
 }
