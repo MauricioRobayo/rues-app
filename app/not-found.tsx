000cb03e2988d87fc92ec61a70ab8030f96c47eb
Mauricio Robayo <rfmajo@gmail.com>
2025-03-03 23:05:21 -0500

    small style adjustment

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -12,13 +12,17 @@ export default function NotFound() {
             Si está buscando una empresa por NIT, recuerde usar sólo números y
             no ingresar el dígito de verificación cuando haga su búsqueda:
           </Text>
-          <Link href="/?nit=">&rarr; Buscar NIT</Link>
+          <Text>
+            &rarr; <Link href="/?nit=">Buscar NIT</Link>
+          </Text>
         </Flex>
         <Flex direction="column" gap="2">
           <Text>
             También puede usar nuestro buscador de empresas por nombre:
           </Text>
-          <Link href="/?razon-social=">&rarr; Buscar Razón Social</Link>
+          <Text>
+            &rarr; <Link href="/?razon-social=">Buscar Razón Social</Link>
+          </Text>
         </Flex>
       </Flex>
     </PageContainer>
