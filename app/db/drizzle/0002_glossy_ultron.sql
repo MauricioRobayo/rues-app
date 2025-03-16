62549a079d1d1bd33da0ae896fb5376eaf6e9629
Mauricio Robayo <rfmajo@gmail.com>
2025-03-16 16:08:23 -0500

    remove drizzle

--- app/db/drizzle/0002_glossy_ultron.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `chambers` ADD `email` text;--> statement-breakpoint
-ALTER TABLE `chambers` ADD `phoneNumber` text;--> statement-breakpoint
-ALTER TABLE `chambers` ADD `certificateUrl` text;--> statement-breakpoint
-ALTER TABLE `chambers` ADD `url` text;
--- app/db/drizzle/0003_handy_gwen_stacy.sql
+++
@@ -1,7 +0,0 @@
-DROP INDEX "chambers_code_unique";--> statement-breakpoint
-DROP INDEX "companies_nit_unique";--> statement-breakpoint
-DROP INDEX "rues_sync_idx";--> statement-breakpoint
-ALTER TABLE `chambers` ALTER COLUMN "code" TO "code" text NOT NULL;--> statement-breakpoint
-CREATE UNIQUE INDEX `chambers_code_unique` ON `chambers` (`code`);--> statement-breakpoint
-CREATE UNIQUE INDEX `companies_nit_unique` ON `companies` (`nit`);--> statement-breakpoint
-CREATE INDEX `rues_sync_idx` ON `companies` (`ruesSyncId`);
