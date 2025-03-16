a5313a8a1bc2c04ec2be2c7504bfa4a144b923a9
Mauricio Robayo <rfmajo@gmail.com>
2025-03-16 15:26:12 -0500

    add revalidate

--- app/sitemap/[id]/route.ts
+++ app/sitemap/[id]/route.ts
@@ -5,6 +5,7 @@ import { getTotalSitemaps } from "@/app/sitemap/getTotalSitemaps";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-static";
+export const revalidate = false;
 
 export async function GET(
   _request: Request,
