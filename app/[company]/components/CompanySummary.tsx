7f485640885d684f254896ce543966625c556427
Mauricio Robayo <rfmajo@gmail.com>
2025-03-17 09:11:23 -0500

    summary text

--- app/[company]/components/CompanySummary.tsx
+++ app/[company]/components/CompanySummary.tsx
@@ -18,7 +18,7 @@ export function companySummary(company: CompanyDto) {
     summary += ` es una ${company.type}`;
   }
 
-  summary += ` que se encuentra ${company.status}`;
+  summary += ` en estado ${company.status}`;
   if (company.cancellationDate) {
     summary += ` que operó hasta el ${company.cancellationDate}`;
   }
