de293879ccb395e4e730983ecac43b4aa7c68348
Mauricio Robayo <rfmajo@gmail.com>
2025-03-17 21:42:16 -0500

    remove unused imports

--- app/services/openData/repository.ts
+++ app/services/openData/repository.ts
@@ -1,10 +1,4 @@
 import { openDataClient } from "@/app/services/openData/client";
-import { Redis } from "@upstash/redis";
-
-const redis = new Redis({
-  url: process.env.KV_REST_API_URL,
-  token: process.env.KV_REST_API_TOKEN,
-});
 
 export const openDataRepository = {
   companyRecords: {
