36bfd5ec3825eaa78abe8c324d3eb6e44d6afe8b
Mauricio Robayo <rfmajo@gmail.com>
2025-03-22 10:11:20 -0500

    show formatter only if number

--- app/mappers/mapOpenDataCompanyRecordToCompanyRecortDto.ts
+++ app/mappers/mapOpenDataCompanyRecordToCompanyRecortDto.ts
@@ -35,7 +35,7 @@ export function mapOpenDataCompanyRecordToCompanyRecordDto(
     isActive: !data.fecha_cancelacion,
     lastRenewalYear: Number(data.ultimo_ano_renovado),
     type: data.tipo_sociedad,
-    legalRepresentatives: getLegalRepresentative(data),
+    legalRepresentative: getLegalRepresentative(data),
     cancellationDate: formatDetailsDate(data.fecha_cancelacion),
     registrationDate: formatDetailsDate(data.fecha_matricula),
     renewalDate: formatDetailsDate(data.fecha_renovacion),
@@ -53,14 +53,12 @@ export function mapOpenDataCompanyRecordToCompanyRecordDto(
 }
 
 function getLegalRepresentative(data: OpenDataCompanyRecord) {
+  console.dir(data, { depth: Infinity });
   return data.representante_legal
-    ? [
-        {
-          type: "principal",
-          name: data.representante_legal,
-          id: data.num_identificacion_representante_legal,
-          idType: data.clase_identificacion_rl,
-        } as const,
-      ]
-    : [];
+    ? ({
+        name: data.representante_legal,
+        id: data.num_identificacion_representante_legal,
+        idType: data.clase_identificacion_rl,
+      } as const)
+    : null;
 }
