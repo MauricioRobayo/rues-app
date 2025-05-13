ba623c655542a14739594506ffeabd049dadbf77
Mauricio Robayo <rfmajo@gmail.com>
2025-05-12 23:07:59 -0500

    logo - shareable title

--- app/[company]/components/CompanyHeader.tsx
+++ app/[company]/components/CompanyHeader.tsx
@@ -1,15 +1,21 @@
 import { CopyButton } from "@/app/[company]/components/CopyButton";
+import { ShareButton } from "@/app/[company]/components/ShareButton";
 import { CompanyStatusBadge } from "@/app/components/CompanyStatusBadge";
 import type { CompanyRecordDto } from "@/app/types/CompanyRecordDto";
-import { Flex, Heading } from "@radix-ui/themes";
+import { Share2Icon } from "@radix-ui/react-icons";
+import { Box, Flex, Heading } from "@radix-ui/themes";
 
 export function CompanyHeader({ company }: { company: CompanyRecordDto }) {
   return (
     <header>
       <Flex direction="column" gap="1">
-        <Heading itemProp="name" color="blue" className="line-clamp-3">
-          {company.name}
-        </Heading>
+        <Box>
+          <ShareButton>
+            <Heading itemProp="name" color="blue" className="line-clamp-3">
+              {company.name} <Share2Icon className="inline size-4" />
+            </Heading>
+          </ShareButton>
+        </Box>
         <Flex
           align={{ initial: "start", sm: "center" }}
           gap="2"
--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -4,7 +4,9 @@ import { Text } from "@radix-ui/themes";
 export function Logo() {
   return (
     <div className={montserrat.className}>
-      <Text size="2">registro</Text>
+      <Text size="4" className="uppercase">
+        Registro
+      </Text>
       <Text size="4" weight="bold">
         NIT
       </Text>
