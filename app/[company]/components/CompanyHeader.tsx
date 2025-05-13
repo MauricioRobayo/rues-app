e32c1060d5eaedd69736230ac762d18963db8e1c
Mauricio Robayo <rfmajo@gmail.com>
2025-05-12 23:58:04 -0500

    cleanup

--- app/[company]/components/CompanyHeader.tsx
+++ app/[company]/components/CompanyHeader.tsx
@@ -1,12 +1,8 @@
-import { CopyButton } from "@/app/[company]/components/CopyButton";
-import {
-  ShareButton,
-  ShareIconButton,
-} from "@/app/[company]/components/ShareButton";
+import { ShareIconButton } from "@/app/[company]/components/ShareButton";
 import { CompanyStatusBadge } from "@/app/components/CompanyStatusBadge";
 import type { CompanyRecordDto } from "@/app/types/CompanyRecordDto";
 import { Share2Icon } from "@radix-ui/react-icons";
-import { Box, Flex, Heading } from "@radix-ui/themes";
+import { Flex, Heading } from "@radix-ui/themes";
 
 export function CompanyHeader({ company }: { company: CompanyRecordDto }) {
   return (
